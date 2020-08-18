229cd832bfa0c154d8258e0be42081a7779a8db1
lavishmanghnani <lavish7939@gmail.com>
2020-08-18 22:04:25 +0530

    timezon all code

---
+++ admin/login.php
@@ -0,0 +1,30 @@
+<?php
+include("connect.php");
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+
+    // username and password sent from form 
+
+    $myusername = mysqli_real_escape_string($db, $_POST['email']);
+    $mypassword = mysqli_real_escape_string($db, $_POST['password']);
+
+    $sql = "SELECT id FROM admin WHERE email = '$myusername' and password = '$mypassword'";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    $count = mysqli_num_rows($result);
+
+    // If result matched $myusername and $mypassword, table row must be 1 row
+
+    if ($count == 1) {
+        echo 'das';
+        $_SESSION["email"] = $myusername;
+        $_SESSION["password"] = $mypassword;
+
+        header("location: users.php");
+    } else {
+        $error = "Your Login Name or Password is invalid";
+    }
+}
---
+++ admin/users.php
@@ -0,0 +1,92 @@
+<?php include_once('section/header.php') ?>
+<?php include_once('section/sidebar.php') ?>
+<?php include('connect.php') ?>
+
+<?php
+
+$sql = "SELECT * FROM `user` ORDER BY id DESC";
+$result = mysqli_query($db, $sql);
+$row = mysqli_fetch_array($result);
+print_r($row);
+die;
+
+?>
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Users List</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Simple Tables</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Bordered Table</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 10px">#</th>
+                                        <th>Name/email/mobile</th>
+                                        <th>Address</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+
+                                    <tr>
+
+                                        <td><?php echo $ginti ?></td>
+                                        <td><?php $row['fname'] ?></td>
+
+
+                                    </tr>
+
+                                </tbody>
+
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer clearfix">
+                            <ul class="pagination pagination-sm m-0 float-right">
+                                <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /.card -->
+
+
+                    <!-- /.card -->
+                </div>
+
+            </div>
+
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+<?php include_once('section/footer.php') ?>
